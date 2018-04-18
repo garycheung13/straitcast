fbc60a8df82be2914d9cc91e44f30821e7df84e4
garycheung13 <garyc102@gmail.com>
2018-04-18 16:58:41 -0400

    heroku deployment fix

--- routes/api.js
+++ routes/api.js
@@ -45,7 +45,7 @@ function parserPromise(body) {
 // firebase init
 admin.initializeApp({
     credential: admin.credential.cert({
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
     databaseURL: process.env.FIREBASE_DB_LINK
